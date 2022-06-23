f9e44045230f887794f2672a5e1f655d069efa57
danielarmond <dani.xavierarmond@gmail.com>
2022-06-23 18:12:10 -0300

    versao 2.2

--- src/main/java/br/com/projeto/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/projeto/forum/config/security/SecurityConfiguration.java
@@ -7,20 +7,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import br.com.projeto.forum.repository.UsuarioRepository;
+
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration{
-
+	
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+    private TokenService tokenService;
+	
+	@Autowired
+    private UsuarioRepository usuarioRepository;
 	
 	@Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
